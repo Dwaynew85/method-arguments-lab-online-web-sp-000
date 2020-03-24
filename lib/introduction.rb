a36a70b2efb5ba6b8f29eb2e10a3eab08e807b9d
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-03-24 23:28:03 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,6 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def intruoduction_with_language
-
+def intruoduction_with_language(name, language)
+  puts "Him, my name is #{name} and I am learning to probgram in #{language}.""
 end
